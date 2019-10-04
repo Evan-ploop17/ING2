3c61ba59caa9c684a57f841b4e70350b23088a83
Esteban Salas <esalas21@uan.edu.co>
2019-10-03 20:35:54 -0500

    Primera version. Esta implementado el boton guardar. No conecta a la base de datos por problmas con los puertos

---
+++ src/Controladores/ConexionSQL.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Esteban
+ */
+public class ConexionSQL {
+
+    Connection conectar = null;
+
+    
+    public Connection conexion()
+    {    
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/nomina","root","Extra12345");
+            
+            JOptionPane.showMessageDialog(null, "Error de conexion");
+            
+        } catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Error de conexion" + e.getMessage());
+            
+        }
+        return conectar;
+    }
+}
